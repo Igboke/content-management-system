6a2fab75a2f8fcd488ce1ed20a80d8edc490c8d8
Igboke Daniel <danieligboke669@gmail.com>
2025-04-22 16:45:28 +0100

    Add custom admin configuration for Article model

--- cms/articles/admin.py
+++ cms/articles/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Article
+from .forms import ArticleForm
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Article model.
+    Uses custom form and adjusts field display.
+    """
+    form = ArticleForm  # Use the custom form defined in forms.py
+    list_display = ('title', 'author', 'created_at', 'updated_at')  # Fields to display in the list view
+    search_fields = ('title', 'author__email')  # Fields to use for searching
+    list_filter = ('created_at', 'updated_at')  # Fields to use for filtering in the sidebar
+    prepopulated_fields = {'slug': ('title',)}  # Automatically populate slug from title
+
+    # Optional: Customize the form layout in the admin interface
+    fieldsets = (
+        (None, {'fields': ('title', 'author', 'slug', 'content', 'picture')}),
+    )
+
+admin.site.register(Article, ArticleAdmin)
