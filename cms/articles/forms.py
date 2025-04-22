9f7998947577fc62234744e1bc430aaf5dc0d407
Igboke Daniel <danieligboke669@gmail.com>
2025-04-22 16:45:39 +0100

    Add ArticleForm for creating and updating Article instances

---
+++ cms/articles/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Article
+
+class ArticleForm(forms.ModelForm):
+    """
+    Form for creating and updating Article instances.
+    """
+    class Meta:
+        model = Article
+        fields = ['title', 'author', 'content', 'picture']  # Include all fields except slug
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 8, 'cols': 40}),  # Customize the content field
+        }
