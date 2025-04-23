a75a53dadc37e63825cc909b92b0bdf54b1f756b
Igboke Daniel <danieligboke669@gmail.com>
2025-04-23 13:02:58 +0100

    Add is_published field to ArticleForm for article publication status

--- cms/articles/forms.py
+++ cms/articles/forms.py
@@ -7,7 +7,7 @@ class ArticleForm(forms.ModelForm):
     """
     class Meta:
         model = Article
-        fields = ['title', 'author', 'content', 'picture']  # Include all fields except slug
+        fields = ['title', 'author', 'content', 'picture','is_published']  # Include all fields except slug
         widgets = {
             'content': forms.Textarea(attrs={'rows': 8, 'cols': 40}),  # Customize the content field
         }
