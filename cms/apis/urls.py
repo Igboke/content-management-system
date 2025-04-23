e883b9fe02efcf21747b516d8d1d6f78bbddf059
Igboke Daniel <danieligboke669@gmail.com>
2025-04-23 22:59:37 +0100

    Add user registration and article search endpoints to API URLs

--- cms/apis/urls.py
+++ cms/apis/urls.py
@@ -1,18 +1,23 @@
 from django.urls import path
-from .views import ArticleViewSet, CommentListCreateAPIView,CommentRetrieveUpdateDestroyAPIView
+from .views import ArticleViewSet, CommentListCreateAPIView,CommentRetrieveUpdateDestroyAPIView, UserRegistrationAPIView, ArticleSearchView, ArticleSearchViewPro
 
 article_list = ArticleViewSet.as_view({
-    'get': 'list'
+    'get': 'list',
+    'post': 'create'
 })
 article_detail = ArticleViewSet.as_view({
     'get': 'retrieve',
     'put': 'update',
-    'delete': 'destroy'
+    'patch': 'partial_update',
+    'delete': 'destroy',
 })
 
 urlpatterns = [
-    path("v1/articles/", article_list, name='article-list'),
-    path("v1/articles/<slug:slug>",article_detail,name='article-detail'),
-    path("v1/articles/<slug:slug>/comments",CommentListCreateAPIView.as_view(),name="comment-list"),
-    path("v1/articles/<slug:slug>/comments/<int:pk>/",CommentRetrieveUpdateDestroyAPIView.as_view(),name="comment-list-create-update-delete")
+    path("v1/user/create/",UserRegistrationAPIView.as_view(),name="create-user"),
+    path("v1/articles/", article_list, name='article-list-create'),
+    path("v1/articles/search/", ArticleSearchView.as_view(),name="article-search"),
+    path("v1/articles/<slug:slug>/",article_detail,name='article-detail'),
+    path("v1/articles/<slug:slug>/comments/",CommentListCreateAPIView.as_view(),name="comment-list-create"),
+    path("v1/articles/<slug:slug>/comments/<int:pk>/",CommentRetrieveUpdateDestroyAPIView.as_view(),name="comment-list-create-update-delete"),
+    path("v1/articles/search/<str:name>/",ArticleSearchViewPro.as_view(),name="article-search-email"),
 ]
