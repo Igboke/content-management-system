8f3ff5533ae181f40abbb12eaa10c2960c07a568
Igboke Daniel <danieligboke669@gmail.com>
2025-04-24 23:06:57 +0100

    Update article search and comment URLs for clarity and consistency

--- cms/apis/urls.py
+++ cms/apis/urls.py
@@ -16,10 +16,10 @@ article_detail = ArticleViewSet.as_view({
 urlpatterns = [
     path("v1/user/create/",UserRegistrationAPIView.as_view(),name="create-user"),
     path("v1/articles/", article_list, name='article-list-create'),
-    path("v1/articles/search/<str:name>/",ArticleSearchViewPro.as_view(),name="article-search-email"),
+    path("v1/articles/search/<str:email>/",ArticleSearchViewPro.as_view(),name="article-search-email"),
     path("v1/articles/search/", ArticleSearchView.as_view(),name="article-search"),
-    path("v1/articles/<slug:slug>/",article_detail,name='article-detail'),
-    path("v1/articles/<slug:slug>/comments/<int:pk>/",CommentRetrieveUpdateDestroyAPIView.as_view(),name="comment-list-create-update-delete"),
+    path("v1/articles/<slug:slug>/",article_detail,name='article-detail-update-delete'),
+    path("v1/articles/<slug:slug>/comments/<int:pk>/",CommentRetrieveUpdateDestroyAPIView.as_view(),name="comment-detail-update-delete"),
     path("v1/articles/<slug:slug>/comments/",CommentListCreateAPIView.as_view(),name="comment-list-create"),
     path("v1/auth/token/",obtain_auth_token,name="obtain-token")
 ]
