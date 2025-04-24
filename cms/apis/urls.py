d5a2b62c464140e8b826403b480d97e4229315eb
Igboke Daniel <danieligboke669@gmail.com>
2025-04-24 16:25:32 +0100

    Add authentication token endpoint and update article comments URL structure

--- cms/apis/urls.py
+++ cms/apis/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import ArticleViewSet, CommentListCreateAPIView,CommentRetrieveUpdateDestroyAPIView, UserRegistrationAPIView, ArticleSearchView, ArticleSearchViewPro
 
 article_list = ArticleViewSet.as_view({
@@ -15,9 +16,10 @@ article_detail = ArticleViewSet.as_view({
 urlpatterns = [
     path("v1/user/create/",UserRegistrationAPIView.as_view(),name="create-user"),
     path("v1/articles/", article_list, name='article-list-create'),
+    path("v1/articles/search/<str:name>/",ArticleSearchViewPro.as_view(),name="article-search-email"),
     path("v1/articles/search/", ArticleSearchView.as_view(),name="article-search"),
     path("v1/articles/<slug:slug>/",article_detail,name='article-detail'),
-    path("v1/articles/<slug:slug>/comments/",CommentListCreateAPIView.as_view(),name="comment-list-create"),
     path("v1/articles/<slug:slug>/comments/<int:pk>/",CommentRetrieveUpdateDestroyAPIView.as_view(),name="comment-list-create-update-delete"),
-    path("v1/articles/search/<str:name>/",ArticleSearchViewPro.as_view(),name="article-search-email"),
+    path("v1/articles/<slug:slug>/comments/",CommentListCreateAPIView.as_view(),name="comment-list-create"),
+    path("v1/auth/token/",obtain_auth_token,name="obtain-token")
 ]
