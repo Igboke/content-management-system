8ecbd59456a3788c189ad145cfb6eddc50b8b1e3
Igboke Daniel <danieligboke669@gmail.com>
2025-04-24 18:55:16 +0100

    Refactor test files to include necessary imports and remove placeholder comments

--- cms/articles/tests.py
+++ cms/articles/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from datetime import timedelta
+from .models import Article, Comment
+from .serializers import ArticlesSerializers, ArticlesSearchSerializer, CommentSerializers
 
-# Create your tests here.
+# Get your custom user model
+CustomUser = get_user_model()
