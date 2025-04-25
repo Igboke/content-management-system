029afc1f7c2bca3e15310695516ca69903c77121
Igboke Daniel <danieligboke669@gmail.com>
2025-04-26 00:02:00 +0100

    Configure email backend settings for user notifications

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -153,3 +153,13 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# removed email host user and password for security reasons
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+SERVER_EMAIL = 'danieligboke669@gmail.com'
