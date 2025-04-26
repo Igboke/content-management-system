1d059fd9423adc2119c8c285d803275c67b7bdc0
Igboke Daniel <102893453+Igboke@users.noreply.github.com>
2025-04-26 20:35:04 +0100

    Update settings.py
    Added render url to allowed hosts

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')#[]
+ALLOWED_HOSTS = ['localhost','127.0.0.1',  'https://content-management-system-rjdu.onrender.com']
 
 
 # Application definition
